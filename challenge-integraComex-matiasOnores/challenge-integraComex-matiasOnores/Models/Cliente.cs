9726e91cace2a4bbc2d9fed06062fc65b8674e58
Matias Onores <matiasonores@gmail.com>
2024-02-01 08:03:43 -0300

    Modelos, controlador y vistas
    Creo el modelo de la clase cliente, acceso DB y vistas en base a la clase modelo

---
+++ challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace challenge_integraComex_matiasOnores.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El CUIT es requerido.")]
+        public long CUIT { get; set; }
+
+        [Required(ErrorMessage = "La razón social es requerida.")]
+        public string RazonSocial { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es requerido.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "El teléfono debe ser numérico.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "La dirección es requerida.")]
+        [StringLength(200, ErrorMessage = "La dirección no puede tener más de 200 caracteres.")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El campo Activo es requerido.")]
+        public bool Activo { get; set; }
+    }
+}
---
+++ challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Models/ClientesDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace challenge_integraComex_matiasOnores.Models
+{
+    public class ClientesDB
+    {
+    }
+}
