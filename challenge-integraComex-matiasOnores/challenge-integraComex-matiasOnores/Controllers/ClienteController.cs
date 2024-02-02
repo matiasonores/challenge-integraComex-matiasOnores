4796ddc17355a1db08ba4a01644666847bd5663c
Matias Onores <matiasonores@gmail.com>
2024-02-02 06:46:21 -0300

    Creacion de ClienteDB y ClienteController
    Desarrollo de los metodos de conexion a base de datos, y primeras pruebas de las vistas

--- challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Controllers/ClienteController.cs
+++ challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Controllers/ClienteController.cs
@@ -1,17 +1,127 @@
-﻿using System;
+﻿using challenge_integraComex_matiasOnores.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
 
 namespace challenge_integraComex_matiasOnores.Controllers
 {
     public class ClienteController : Controller
     {
         // GET: Cliente
+        [HttpGet]
         public ActionResult Index()
         {
+            //List<Cliente> clientes = new List<Cliente>();
+
+            var clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    Id = 1,
+                    CUIT = 12345678901,
+                    RazonSocial = "Empresa A",
+                    Telefono = "123456789",
+                    Direccion = "Calle 123",
+                    Activo = true
+                },
+                new Cliente
+                {
+                    Id = 2,
+                    CUIT = 98765432109,
+                    RazonSocial = "Empresa B",
+                    Telefono = "987654321",
+                    Direccion = "Avenida 456",
+                    Activo = false
+                }
+                // Puedes agregar más clientes aquí si lo deseas
+            };
+            return View(clientes);
+        }
+        // POST: Cliente/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente nuevo)
+        {
+            if (ModelState.IsValid)
+            {
+                //using (SqlConnection connection = new SqlConnection(""))
+                //{
+                //    connection.Open();
+                //    using (SqlCommand cmd = new SqlCommand("NombreDeTuStoredProcedure", connection))
+                //    {
+                //        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                //        cmd.Parameters.AddWithValue("@Parametro1", nuevo.CUIT);
+                //        cmd.Parameters.AddWithValue("@Parametro2", nuevo.RazonSocial);
+                //        cmd.Parameters.AddWithValue("@Parametro3", nuevo.Direccion);
+                //        cmd.Parameters.AddWithValue("@Parametro4", nuevo.Telefono);
+                //        cmd.Parameters.AddWithValue("@Parametro5", nuevo.Activo);
+
+                //        cmd.ExecuteNonQuery();
+                //    }
+                //}
+                //return RedirectToAction("Index", "Cliente");
+            }
+
+            //return View(nuevo);
+
+            return Json(new { success = true, message = "Cliente creado exitosamente" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Edit()
+        {
+            Cliente cliente = new Cliente
+            {
+                Id = 2,
+                CUIT = 98765432109,
+                RazonSocial = "Empresa B",
+                Telefono = "987654321",
+                Direccion = "Avenida 456",
+                Activo = false
+            };
+            return View(cliente);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+
             return View();
+
+        }
+
+        [HttpPost]
+        public async Task<string> GetNombreByCuitAsync(string cuit)
+        {
+            var retorno = "";
+
+            try
+            {
+                string endpoint = "https://sistemaintegracomex.com.ar/Account/GetNombreByCuit?cuit=" + cuit;
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Cookie", "ASP.NET_SessionId=jkl53zqtqajie4vee051xzta");
+                HttpResponseMessage response = await client.PostAsync(endpoint, new StringContent("", null, "text/plain"));
+                response.EnsureSuccessStatusCode();
+                client.Dispose();
+                retorno = await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                retorno = ex.Message;
+            }
+
+            return retorno;
         }
     }
 }
--- challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Models/ClientesDB.cs
+++ challenge-integraComex-matiasOnores/challenge-integraComex-matiasOnores/Models/ClientesDB.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,204 @@ namespace challenge_integraComex_matiasOnores.Models
 {
     public class ClientesDB
     {
+        private string _connectionString;
+        private readonly SqlConnection _connection;
+
+        private SqlCommand _command;
+
+        public ClientesDB()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            _connection = new SqlConnection(_connectionString);
+        }
+        private void AbrirConexion()
+        {
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        private void CerrarConexion()
+        {
+            if (_connection.State != ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+        
+        private void SetStoredProcedure(string nombreStoredProcedure)
+        {
+            try
+            {
+                _command = new SqlCommand();
+                _command.Connection = _connection;
+                _command.CommandText = nombreStoredProcedure;
+                _command.CommandType = CommandType.StoredProcedure;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void SetQuery(string query)
+        {
+            try
+            {
+                _command = new SqlCommand();
+                _command.Connection = _connection;
+                _command.CommandText = query;
+                _command.CommandType = CommandType.Text;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void SetParameter(string parametro, object valor)
+        {
+            try
+            {
+                _command.Parameters.AddWithValue(parametro, valor);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private DataTable EjecutarComando()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                AbrirConexion();
+                SqlDataAdapter adapter = new SqlDataAdapter(_command);
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+
+            return dt;
+        }
+        public DataTable ObtenerClientes()
+        {
+            DataTable dt;
+            try
+            {
+                string nombreStoredProcedure = "ObtenerClientes";
+                SetStoredProcedure(nombreStoredProcedure);
+                dt = EjecutarComando();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en ObtenerClientes(): " + ex.Message);
+            }
+
+            return dt;
+        }
+
+        public DataTable ObtenerClientePorId(int id)
+        {
+            DataTable dt;
+            try
+            {
+                string nombreStoredProcedure = "ObtenerClientePorId";
+                SetStoredProcedure(nombreStoredProcedure);
+                SetParameter("@id", id);
+                dt = EjecutarComando();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en ObtenerClientePorId("+ id +"): " + ex.Message);
+            }
+
+            return dt;
+        }
+
+        public bool CrearCliente(Cliente cliente)
+        {
+            bool creado = false;
+            try
+            {
+                string nombreStoredProcedure = "CrearCliente";
+                SetStoredProcedure(nombreStoredProcedure);
+                SetParameter("@cuit", cliente.CUIT);
+                SetParameter("@razonSocial", cliente.RazonSocial);
+                SetParameter("@telefono", cliente.Telefono);
+                SetParameter("@direccion", cliente.Direccion);
+                SetParameter("@activo", cliente.Activo);
+                DataTable resultado = EjecutarComando();
+
+                // Verificar si la consulta devolvió al menos una fila
+                creado = resultado != null && resultado.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en CrearCliente: " + ex.Message);
+            }
+
+            return creado;
+
+        }
+
+        public bool ModificarCliente(Cliente cliente)
+        {
+            bool modificado = false;
+            try
+            {
+                string nombreStoredProcedure = "ModificarCliente";
+                SetStoredProcedure(nombreStoredProcedure);
+                SetParameter("@id", cliente.Id);
+                SetParameter("@cuit", cliente.CUIT);
+                SetParameter("@razonSocial", cliente.RazonSocial);
+                SetParameter("@telefono", cliente.Telefono);
+                SetParameter("@direccion", cliente.Direccion);
+                SetParameter("@activo", cliente.Activo);
+                DataTable resultado = EjecutarComando();
+
+                modificado = resultado != null && resultado.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en ModificarCliente: " + ex.Message);
+            }
+            return modificado;
+        }
+
+        public bool EliminarCliente(int id)
+        {
+            bool eliminado = false;
+            try
+            {
+                string nombreStoredProcedure = "EliminarCliente";
+                SetStoredProcedure(nombreStoredProcedure);
+                SetParameter("@id", id);
+                DataTable resultado = EjecutarComando();
+
+                // Verificar si la consulta devolvió al menos una fila
+                eliminado = resultado != null && resultado.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en EliminarCliente: " + ex.Message);
+            }
+
+            return eliminado;
+        }
+
+
+
     }
 }
